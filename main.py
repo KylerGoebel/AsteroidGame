f879c29c312b5ab7bed52328fc1b962749b792a5
KylerGoebel <InterstellarLucidDream@gmail.com>
2026-04-12 15:18:08 -0400

    init game, fill screen, and ability to close game.

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}\n"
+          f" Screen width: {SCREEN_WIDTH} \n Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+             pass
+        
+        screen.fill("black")
+
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
