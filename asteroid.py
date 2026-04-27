b8e0b002cc10fdca6de8e323c2ac6b62a63e5ad1
KylerGoebel <InterstellarLucidDream@gmail.com>
2026-04-27 15:56:42 -0400

    Add all project files

---
+++ asteroid.py
@@ -0,0 +1,32 @@
+from constants import *
+from circleshape import CircleShape
+import pygame
+from logger import log_state ,log_event
+import random
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            angle = random.uniform(20, 50)
+            new_pos_velocity = self.velocity.rotate(angle) * 1.2
+            new_neg_velocity = self.velocity.rotate(-angle) * 1.2
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+            asteroid1.velocity = new_pos_velocity
+            asteroid2.velocity = new_neg_velocity
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state ,log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\n"
@@ -8,18 +13,59 @@ def main():
     
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    asteroidfield = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+    
 
     while True:
         log_state()
 
         for event in pygame.event.get():
-             pass
+             if event.type == pygame.QUIT:
+                return
         
+        updatable.update(dt)
+
         screen.fill("black")
+
+        for drawable_sprite in drawable:
+            drawable_sprite.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
 
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS and get delta time in seconds
+
 
 
 if __name__ == "__main__":
